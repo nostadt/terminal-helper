658afd17dbd0090b1961e4d93fa668efc2749c7f
Alexander Nostadt <a.nostadt@constructiva.de>
2019-08-01 08:35:21 +0200

    Add more aliases

---
+++ dev/.git.sh
@@ -0,0 +1,5 @@
+alias git-graph='git log --all --decorate --oneline --graph'
+alias git-amend='git commit --amend'
+alias git-push-with-tags='git push && git push --tags'
+alias git-master='git checkout master'
+alias git-develop='git checkout develop'
