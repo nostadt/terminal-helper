fefb6ae82d032c6bf62f3e65ce05899decc7efb3
Alexander Nostadt <alexander@nostadt.de>
2019-08-29 13:41:49 +0200

    [TASk] gitUpdateLocalRepository returns to current branch now

--- dev/functions/git.sh
+++ dev/functions/git.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 function gitBranch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1) /'
+}
+
+function gitBranchSimple() {
+  git branch | grep "\*" | sed -e 's/* //g'
 }
 
 function gitUpdateLocalRepository() {
+  currentBranch=$(gitBranchSimple)
+  echo "Checking out master...";
   git checkout master
+  echo "Pulling latest commits from remote master...";
   git pull
+  echo "Checking out develop...";
   git checkout develop
+  echo "Pulling latest commits from remote develop...";
   git pull
+  echo "Switch back to original branch...";
+  git checkout $currentBranch
 }
