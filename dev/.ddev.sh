2faf2aa3055c164a9e77f86df897e6ff5055d7f3
Alexander Nostadt <AMartinNo1@users.noreply.github.com>
2020-03-13 16:50:03 +0100

    2 shortcuts to open PMA and MailHog from CLI

--- dev/.ddev.sh
+++ dev/.ddev.sh
@@ -2,3 +2,6 @@ alias dup='ddev start'
 alias dauth='ddev auth ssh'
 alias dhalt='ddev stop'
 alias drestart='ddev restart'
+
+alias ddev-pma="ddev describe | grep 'phpMyAdmin' | awk '{print \$NF}' | xargs open"
+alias ddev-mailhog="ddev describe | grep 'MailHog' | awk '{print \$NF}' | xargs open"
