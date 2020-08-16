156f6f0917ae4d5c95eb0b8b2bca7bc44477535a
Alexander Nostadt <alexander@nostadt.de>
2020-08-16 10:27:47 +0200

    !!! [TASK] #4 Drop -pma and -mailhog alias in favor of core feature

--- scripts/.ddev.sh
+++ scripts/.ddev.sh
@@ -2,6 +2,3 @@ alias dup='ddev start'
 alias dauth='ddev auth ssh'
 alias dhalt='ddev stop'
 alias drestart='ddev restart'
-
-alias ddev-pma="ddev describe | grep 'phpMyAdmin' | awk '{print \$NF}' | xargs open"
-alias ddev-mailhog="ddev describe | grep 'MailHog' | awk '{print \$NF}' | xargs open"
